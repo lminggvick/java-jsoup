805f5086e67947dd06015dc3d2a99ac1f16ed1a5
lminggvick <lminggvick@naver.com>
2019-09-25 11:23:38 +0900

    Add: 유효 게시글 키워드 추가

--- src/main/java/Model/ValidKeyword.java
+++ src/main/java/Model/ValidKeyword.java
@@ -4,7 +4,11 @@ import Interface.TypeMapper;
 
 public enum ValidKeyword implements TypeMapper {
     KEY1("경상도지역 직거래"),
-    KEY2("[직거래]아파트.전세.지방");
+    KEY2("[직거래]아파트.월세.지방"),
+    KEY4("[직거래]아파트.전세.지방"),
+    KEY5("[직거래]아파트.매매.지방"),
+    KEY6("음식점 / 식당 / 분식 "),
+    KEY7("UNKNOWN");
 
     private String name;
 
